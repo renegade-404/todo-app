014346c9c4fb3b7a23455ca9898b870138f41f95
renegade-404 <mypcisrenegade@gmail.com>
2025-08-12 08:20:07 +0900

    fix naming inconsistencies which caused bugs; change containers creation functionality to be the same as DOM elements

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -7,41 +7,45 @@ function renderTasks(tasksArray) {
 }
 
 function createInputPopup(popupType) {
-    const popupDomElements = [
-        { element: "input", type: "text", id: "name", name: "name", placeholder: `${popupType} name`, container: "popupTopContainer" },
-        { element: "input", type: "date", id: "due", name: "due", container: "popupBottomContainer" },
-        { element: "section", id: "priority", name: "priority", container: "popupBottomContainer"},
-        { element: "button", text: `Add ${popupType}`, elClass: `${popupType}-input-btn`, container: "popupBottomContainer"},
+    const bodyEl = document.body;
+
+    const popupContainers = [
+        { tag: "div", elClass: "popup-main-cont", container: bodyEl},
+        { tag: "div", elClass: "popup-sub-top-cont", container: "popup-main-cont"},
+        { tag: "div", elClass: "popup-sub-bot-cont", container: "popup-main-cont"},
     ];
 
-    const popupContainer = document.createElement("div");
-    popupContainer.classList.add("popup-container"); // todo: add position: absolute and position: relative
-    document.body.appendChild("popup");
+    const popupDomElements = [
+        { tag: "input", type: "text", id: "name", name: "name", placeholder: `${popupType} name`, container: "popup-sub-top-cont" },
+        { tag: "input", type: "date", id: "due", name: "due", container: "popup-sub-bot-cont" },
+        { tag: "section", id: "priority", name: "priority", container: "popup-sub-bot-cont"},
+        { tag: "button", text: `Add ${popupType}`, elClass: `${popupType}-input-btn`, container: "popup-sub-bot-cont"},
+    ];
 
-    const popupTopContainer = document.createElement("div");
-    popupContainer.appendChild(popupTopContainer);
-
-    const popupBottomContainer = document.createElement("div");
-    popupContainer.appendChild(popupBottomContainer);
+    createDomElements(popupContainers);
 
     createDomElements(popupDomElements);
     
 }
 
-
 function createDomElements(arr) {
 
-    arr.forEach(({tag, type, id, name, placeholder, elClass, container}) => {
+    arr.forEach(({tag, type, id, name, placeholder, elClass, container, text}) => {
         const element = document.createElement(tag);
         if (type) element.type = type;
         if (id) element.id = id;
         if (name) element.name = name;
         if (placeholder) element.placeholder = placeholder;
         if (elClass) element.classList.add(elClass);
-        if (text) el.textContent = text;
+        if (text) element.textContent = text;
 
-        const targetContainer = document.querySelector(`.${container}`);
-        targetContainer.appendChild(element);
+        if (elClass == "popup-main-cont") {
+            container.appendChild(element);
+        } else {
+            const targetContainer = document.querySelector(`.${container}`);
+            targetContainer.appendChild(element);
+        }
+        
     })
 }
 
