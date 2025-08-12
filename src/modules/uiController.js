766380502a519f81aa94d53015253c8824a2114d
renegade-404 <mypcisrenegade@gmail.com>
2025-08-13 08:19:46 +0900

    fix naming; change div to from

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -10,21 +10,25 @@ function createInputPopup(popupType) {
     const bodyEl = document.body;
 
     const popupContainers = [
-        { tag: "div", elClass: "popup-main-cont", container: bodyEl},
-        { tag: "div", elClass: "popup-sub-top-cont", container: "popup-main-cont"},
-        { tag: "div", elClass: "popup-sub-bot-cont", container: "popup-main-cont"},
+        { tag: "form", elClass: "popup-form", container: bodyEl},
+        { tag: "div", elClass: "popup-top-container", container: "popup-form"},
+        { tag: "div", elClass: "popup-bot-container", container: "popup-form"},
     ];
 
     const popupDomElements = [
-        { tag: "input", type: "text", id: "name", name: "name", placeholder: `${popupType} name`, container: "popup-sub-top-cont" },
-        { tag: "input", type: "date", id: "due", name: "due", container: "popup-sub-bot-cont" },
-        { tag: "section", id: "priority", name: "priority", container: "popup-sub-bot-cont"},
-        { tag: "button", text: `Add ${popupType}`, elClass: `${popupType}-input-btn`, container: "popup-sub-bot-cont"},
+        { tag: "input", type: "text", id: "name", name: "name", placeholder: `${popupType} name`, container: "popup-top-container" },
+        { tag: "input", type: "date", id: "due", name: "due", container: "popup-bot-container" },
+        { tag: "select", id: "priority", name: "priority", container: "popup-bot-container"},
+        { tag: "button", type: "submit", text: `Add ${popupType}`, elClass: `${popupType}-input-btn`, container: "popup-bot-container"},
     ];
+
+    const selectElementOptions = ["important", "semi-important", "normal"];
 
     createDomElements(popupContainers);
 
     createDomElements(popupDomElements);
+
+    addSelectOption(selectElementOptions, "priority");
     
 }
 
@@ -39,13 +43,22 @@ function createDomElements(arr) {
         if (elClass) element.classList.add(elClass);
         if (text) element.textContent = text;
 
-        if (elClass == "popup-main-cont") {
+        if (elClass == "popup-form") {
             container.appendChild(element);
         } else {
             const targetContainer = document.querySelector(`.${container}`);
             targetContainer.appendChild(element);
         }
         
+    })
+}
+
+function addSelectOption(arr, selectId) {
+    const selectEl = document.querySelector(`#${selectId}`)
+    arr.forEach(item => {
+        const optionEl = document.createElement("option");
+        optionEl.innerText = item;
+        selectEl.appendChild(optionEl);
     })
 }
 
