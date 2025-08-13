e865ab16377de25a432cb54513369271bc333b1e
renegade-404 <mypcisrenegade@gmail.com>
2025-08-14 08:20:39 +0900

    create popup handler

--- src/modules/eventController.js
+++ src/modules/eventController.js
@@ -2,10 +2,24 @@ export const eventControl = (() => {
     const addSelectionBtn = document.querySelector(".add-with-select-btn");
     const addProjectBtn = document.querySelector(".add-project-btn");
     const addTaskBtn = document.querySelector(".add-task-btn");
+    const inputsObj = {};
 
-    function handler(clickType) {
-        
+    function popupHandler() {
+        const inputsObj = {};
+        const inputs = document.querySelectorAll(".popup-input");
+
+        inputs.forEach((item) => {
+            inputsObj[item.id] = item.value;
+        })
     }
 
+    function mainPageHandler(e, fn) {
+        const itemType = e.target.classList[0].split("-input-btn")[0];
+    }
+
+
+
+
+    return { inputsObj }
 
 })()
