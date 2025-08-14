650adb338f1546c4e19e2dcd4bc16fe5523c693f
renegade-404 <mypcisrenegade@gmail.com>
2025-08-15 08:16:31 +0900

    complete main page handler and popup handler

--- src/modules/eventController.js
+++ src/modules/eventController.js
@@ -1,25 +1,37 @@
 export const eventControl = (() => {
-    const addSelectionBtn = document.querySelector(".add-with-select-btn");
+    const addSelectionBtn = document.querySelector(".add-select-btn");
     const addProjectBtn = document.querySelector(".add-project-btn");
     const addTaskBtn = document.querySelector(".add-task-btn");
+    const arrOfBtns = [addSelectionBtn, addProjectBtn, addTaskBtn];
+
     const inputsObj = {};
 
+
     function popupHandler() {
-        const inputsObj = {};
+        for (let key in inputsObj) {
+            delete inputsObj[key];
+        }
+        
         const inputs = document.querySelectorAll(".popup-input");
+        const inputsForm = document.querySelector(".input-form");
 
         inputs.forEach((item) => {
             inputsObj[item.id] = item.value;
         })
+
+        document.body.removeChild(inputsForm);
     }
 
     function mainPageHandler(e, fn) {
-        const itemType = e.target.classList[0].split("-input-btn")[0];
+        const itemType = e.target.classList[0].split("-")[1];
+        fn(itemType);
+
+        const inputBtn = document.querySelector(`.${itemType}-input-btn`);
+        inputBtn.addEventListener("click", popupHandler);
+
+
     }
 
+    return { inputsObj, arrOfBtns, mainPageHandler }
 
-
-
-    return { inputsObj }
-
-})()
+})();
