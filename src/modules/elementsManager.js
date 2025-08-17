3d07bd3e7dc6e37873fdf68fd34038de2710d6d6
renegade-404 <mypcisrenegade@gmail.com>
2025-08-17 14:11:46 +0900

    refactor objects - split into two

--- src/modules/elementsManager.js
+++ src/modules/elementsManager.js
@@ -1,4 +1,18 @@
+import { renderProjects, renderTasks } from "./uiController";
+import { dataManagement } from "./dataManager";
+
+const dataManager = dataManagement();
+
 export const domElements = {
-    sidebarProjList: document.querySelector(".sidebar-projects-list"),
+    sidebarProjectsList: document.querySelector(".sidebar-projects-list"),
     sidebarTasksList: document.querySelector(".sidebar-tasks-list"),
+}
+
+export const functionsList = {
+    rendTasks: renderTasks,
+    rendProjects: renderProjects,
+    getTasks: dataManager.getTasks,
+    getProjects: dataManager.getProjects,
+    addTask: dataManager.addTask,
+    addProject: dataManager.addProject,
 }
