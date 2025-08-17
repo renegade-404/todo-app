9a1755734a219f002901d7220b0582042063d4e0
renegade-404 <mypcisrenegade@gmail.com>
2025-08-17 14:15:14 +0900

    refactor functions - adjust importing, add necessary import functions to the event handlers

--- src/modules/eventController.js
+++ src/modules/eventController.js
@@ -1,3 +1,5 @@
+import { domElements, functionsList } from "./elementsManager";
+
 export const eventControl = (() => {
     const addSelectionBtn = document.querySelector(".add-select-btn");
     const addProjectBtn = document.querySelector(".add-project-btn");
@@ -7,28 +9,42 @@ export const eventControl = (() => {
     const inputsObj = {};
 
 
-    function popupHandler() { //add rendering function
-        for (let key in inputsObj) {
-            delete inputsObj[key];
+    function objCreationHandler(e, renderFn, getArray, getContainer) { // creates task or project object
+        try {
+            for (let key in inputsObj) {
+                delete inputsObj[key];
+            }
+
+            const inputs = document.querySelectorAll(".popup-input");
+            const inputsForm = document.querySelector(".popup-form");
+            const itmType = e.target.classList[0].split("-")[0];
+            const typeAsArg = itmType[0].toUpperCase() + itmType.slice(1, itmType.length);
+
+            inputs.forEach((item) => {
+                inputsObj[item.id] = item.value;
+            })
+
+            document.body.removeChild(inputsForm);
+
+            functionsList[`add${typeAsArg}`](inputsObj);
+            renderFn(getArray(), getContainer);
+        } catch (er) {
+            throw er;
         }
-        
-        const inputs = document.querySelectorAll(".popup-input");
-        const inputsForm = document.querySelector(".input-form");
-
-        inputs.forEach((item) => {
-            inputsObj[item.id] = item.value;
-        })
-
-        document.body.removeChild(inputsForm);
-
+    
     }
 
-    function mainPageHandler(e, createInputFn) {
+    function mainPageHandler(e, createInputPopFn) {
         const itemType = e.target.classList[0].split("-")[1];
-        createInputFn(itemType);
+        const typeAsArg = itemType[0].toUpperCase() + itemType.slice(1, itemType.length) + "s";
+        const getRendFn = functionsList[`rend${typeAsArg}`];
+        const getArr = functionsList[`get${typeAsArg}`];
+        const getCont = domElements[`sidebar${typeAsArg}List`];
+
+        createInputPopFn(itemType);
 
         const inputBtn = document.querySelector(`.${itemType}-input-btn`);
-        inputBtn.addEventListener("click", popupHandler);
+        inputBtn.addEventListener("click", (e) => objCreationHandler(e, getRendFn, getArr, getCont));
 
     }
 
